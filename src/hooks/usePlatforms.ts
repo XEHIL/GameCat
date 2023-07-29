9abfdb9991da57ccf5778a8030deeaaffd95537d
xehil <65438399+xehil@users.noreply.github.com>
2023-07-29 15:56:56 +0530

    Removed unused imports

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,11 @@
 import platforms from "../data/platfroms";
 //import useData from "./useData";
 
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+// interface Platform {
+//   id: number;
+//   name: string;
+//   slug: string;
+// }
 
 //const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 
